4eb74ecd799efacead3a91a0bff76094827278b7
adsons3c <adsonemanuels3c@gmail.com>
2018-09-30 07:58:33 -0300

    Primeiros testes

---
+++ superlist/list/tests.py
@@ -0,0 +1,9 @@
+from django.urls import resolve
+from django.test import TestCase
+from list.views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolve(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
