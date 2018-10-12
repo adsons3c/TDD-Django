b6699d864f44531372c313c1767df2e2a04b9093
adsons3c <adsonemaneuls3c@gmail.com>
2018-10-11 21:05:13 -0300

    refatorando

--- functional_tests.py
+++ functional_tests.py
@@ -19,24 +19,28 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         inputbox = self.browser.find_element_by_id('id_new_iten')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-        inputbox.send_keys('Buy peacock feathers')
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(1)
 
-        self.assertIn('To-Do', header_text)
+        # self.assertIn('To-Do', header_text)
         # self.fail('Finish the test')
 
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        # self.assertTrue(
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
+
+        self.fail('Finish the test')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
